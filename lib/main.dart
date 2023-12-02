7e6e79c0bf44d706dbb85be93bb4aa0cc9a1d105
Faidkeddouri-afk <faid.keddouri@ensia.edu.dz>
2023-12-02 14:50:36 +0100

    Add the skeleton widget of the task card and the task page need some slight modification , also modified the main a little and it needs some fixing

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:study_buddy/domain/entities/user_entity.dart';
 import 'package:study_buddy/presentation/pages/community/community.dart';
+import 'package:study_buddy/presentation/pages/tasks_list/tasks.dart';
 
 import 'core/theme/color_scheme.dart';
 import 'presentation/widgets/skeleton/navigation_bar.dart';
 import 'presentation/widgets/skeleton/top_bar.dart';
+import 'presentation/widgets/skeleton/task_card.dart';
 
 void main() {
   const UserEntity user = UserEntity(
@@ -13,7 +15,7 @@ void main() {
     progress: 70,
   );
 
-  runApp(const MainApp(
+  runApp(MainApp(
     user: user,
   ));
 }
@@ -28,21 +30,69 @@ class MainApp extends StatefulWidget {
 
 class _MainAppState extends State<MainApp> {
   int pageIndex = 0;
-  final List<Widget> pages = [const Community()];
+  late final List<Widget> pages;
+
+  @override
+  void initState() {
+    super.initState();
+    pages = [
+      const Community(),
+      TasksPage(user: widget.user), // Pass 'widget.user' to the TasksPage
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        // theme: themeData,
-        home: SafeArea(
-          child: Scaffold(
-              backgroundColor: MyColorScheme.backgroundColor,
-              appBar: TopBar(user: widget.user),
-              body: Stack(
-                alignment: Alignment.bottomCenter,
-                children: [pages[pageIndex], const MyNavigationBar()],
-              )),
-        ));
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: MyColorScheme.backgroundColor,
+          appBar:
+              TopBar(user: widget.user), // TopBar takes 'user' as a parameter
+          body: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              // IndexedStack is used to maintain the state of the off-screen page
+              IndexedStack(
+                index: pageIndex,
+                children: pages,
+              ),
+              MyNavigationBar(onPageSelected: (index) {
+                setState(() {
+                  pageIndex = index;
+                });
+              }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyNavigationBar extends StatelessWidget {
+  final Function(int) onPageSelected;
+  const MyNavigationBar({super.key, required this.onPageSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    final _MainAppState state =
+        context.findAncestorStateOfType<_MainAppState>()!;
+
+    return BottomNavigationBar(
+      currentIndex: state.pageIndex,
+      onTap: onPageSelected, // Use the passed function to handle page switching
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.people),
+          label: 'Community',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.list),
+          label: 'Tasks',
+        ),
+      ],
+    );
   }
 }
